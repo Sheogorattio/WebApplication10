eb9c5afd22366a4b2a180acab76fba3a9cfc8806
Sheogorattio <91422100+Sheogorattio@users.noreply.github.com>
2024-04-01 04:12:08 +0300

    Add project files.

---
+++ WebApplication10/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication10.Models;
+
+namespace WebApplication10.Controllers
+{
+    public class User
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            User user = new()
+            {
+                Name = "John Doe",
+                Email = "john.doe@example.com"
+            };
+
+            ViewBag.UserName = user.Name;
+            ViewBag.UserEmail = user.Email;
+
+            ViewData["UserName"] = user.Name;
+            ViewData["UserEmail"] = user.Email;
+
+            return View(user);
+        }
+    }
+
+
+}
